5e0be0188632040a06890b7ae342b6c488b13f69
DanielAdek <maildaniel.me1@gmail.com>
2020-07-22 16:06:52 +0100

    feate: add answer functionality route

--- src/routes/StackRoutes.ts
+++ src/routes/StackRoutes.ts
@@ -7,11 +7,17 @@ class StackRoutes {
   constructor() {
     this._router = Router();
     this.questionEndpoint();
+    this.answerEndpoint();
   }
 		
   private questionEndpoint = (): void => {
     this._router.post('/question/create', StackQuestion.createQuestion);
     this._router.get('/question', StackQuestion.retrieveQuestion);
+  }
+
+  private answerEndpoint = (): void => {
+    this._router.post('/question/:questionId/answer', StackQuestion.createQuestion);
+    this._router.get('/answer/:answerId/like', StackQuestion.retrieveQuestion);
   }
 }
 export default new StackRoutes();
