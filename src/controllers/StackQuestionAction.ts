359383c80836eb196e338211d86981838d9b292a
DanielAdek <maildaniel.me1@gmail.com>
2020-07-22 15:42:31 +0100

    feat: add retrieve functionality

--- src/controllers/StackQuestionAction.ts
+++ src/controllers/StackQuestionAction.ts
@@ -5,15 +5,16 @@ import { ResponseFormat, successResponse, errorResponse } from '@modules/util/re
 import { formSchema } from '@modules/validation/schema';
 import db from '@models/index';
 import * as Messanger from '@modules/services/StackService';
+import { StackQuestions } from '@modules/models/StackQuestions';
 
 /**
  * @class StackQuestion
  */
 export class StackQuestion {
   /**
-   * @method create
-   * @desc Feature create user account
    * @author DanielAdek
+   * @method createQuestion
+   * @desc Feature create user account
    * @param {object} req Request object
    * @param {object} res Response object
    * @returns {object} Json data
@@ -32,13 +33,15 @@ export class StackQuestion {
       const foundQues = await Messanger.shouldFindOneObject(db.StackQuestionsDB, { questionaire, title });
 
       if (foundQues) {
-        const result = errorResponse('ERRDuplicateRecord', 422, 'ques', 'create question', 'You have asked this question already!', { error: true, operationStatus: 'Proccess Terminated!' });
+        const result = errorResponse('ErrDuplicateRecord', 422, 'ques', 'create question', 'You have asked this question already!', { error: true, operationStatus: 'Proccess Terminated!' });
         return res.status(422).jsend.fail(result);
       }
 
-      const asked = moment(new Date()).format('ll');
+      const asked = moment(new Date()).fromNow();
 
-      const data = { questionaire, title, ques, tags, asked, active: asked };
+      const arrayOfTags = tags.split(',').map((x: string) => x.trim());
+
+      const data = { questionaire, title, ques, tags: arrayOfTags, asked };
 
       const question = await Messanger.shouldInsertToDataBase(db.StackQuestionsDB, data);
 
@@ -47,6 +50,38 @@ export class StackQuestion {
       return res.status(201).jsend.success(result);
     } catch (error) {
       const result = errorResponse(`${error.syscall || error.name || 'ServerError'}`, 500, `${error.path || 'No Field'}`, 'create question', `${error.message}`, { error: true, operationStatus: 'Proccess Terminated!', errorSpec: error });
+      return res.status(500).jsend.fail(result);
+    }
+  }
+
+  /**
+   * @author DanielAdek
+   * @method retrieveQuestions
+   * @desc Feature create user account
+   * @param {object} req Request object
+   * @param {object} res Response object
+   * @returns {object} Json data
+   */
+   public static retrieveQuestion = async (req: Request, res: Response): Promise<ResponseFormat | any> => {
+     try {
+      const foundQues = <StackQuestions> await Messanger.shouldFindOneObject(db.StackQuestionsDB, { title: req.query.search });
+
+      if (!foundQues) {
+        const result = errorResponse('ErrDocNotFound', 404, 'ques', 'retrieve question', 'Question does not exist', { error: true, operationStatus: 'Proccess Terminated!' });
+        return res.status(404).jsend.fail(result);
+      }
+
+      foundQues.viewed += 1;
+
+      foundQues.asked = moment((foundQues as StackQuestions).createdAt).fromNow();
+
+      await foundQues.save();
+
+      const result = successResponse('Question retrieved Successfully', 200, 'retrieve question', { error: false, operationStatus: 'Proccess Completed!', foundQues });
+
+      return res.status(200).jsend.success(result);
+    } catch (error) {
+      const result = errorResponse(`${error.syscall || error.name || 'ServerError'}`, 500, `${error.path || 'No Field'}`, 'retrieve question', `${error.message}`, { error: true, operationStatus: 'Proccess Terminated!', errorSpec: error });
       return res.status(500).jsend.fail(result);
     }
   }
